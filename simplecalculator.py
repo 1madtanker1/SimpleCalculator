2388b594cd05bcfa3c3ef39a8fe243304a1d5683
1madtanker1 <87938019+1madtanker1@users.noreply.github.com>
2021-12-24 15:17:55 -0400

    Create simplecalculator.py

---
+++ simplecalculator.py
@@ -0,0 +1,7 @@
+#Calculator
+#this was a quick project I learned from watching python videos
+num1 = input("Enter a number > ")
+num2 = input ("Enter another number > ")
+result = float(num1) + float(num2)
+
+print(result)
