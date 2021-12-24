7f659e25c6400eff24257a414abf682b9166cb03
1madtanker1 <87938019+1madtanker1@users.noreply.github.com>
2021-12-24 15:33:35 -0400

    Create Madlibsgame.py

---
+++ Madlibsgame.py
@@ -0,0 +1,10 @@
+#This is a mad libs game
+#I designed this while distening to a python for beginners tutorial
+#This is really stupid
+color = input ("Enter a color: ")
+favorite_car = input("Enter your favorite car: ")
+plural_noun = input("Enter a plural noun: ")
+
+print("My car is " + color)
+print(favorite_car + " is blue")
+print("This is a " + plural_noun)
